dfce4b73d392e73f9f5c95ee57388c0341285943
VisShon <vishnu20414@iiitd.ac.in>
2022-07-25 16:39:12 +0530

    photo slider implemented

--- frontend/src/components/PhotoSlider.tsx
+++ frontend/src/components/PhotoSlider.tsx
@@ -1,34 +1,26 @@
-import React, {useState , useEffect} from 'react';
+import React, {useState , useRef} from 'react';
 import '../styles/PhotoSlider.scss'
-import {FaArrowAltCircleRight , FaArrowAltCircleLeft} from 'react-icons/fa';
+import left from '../images/left.png'
+import right from '../images/right.png'
+
 function PhotoSlider({data}:any) {
-
-  const [current , setCurrent]  = useState(0);
-
-  const showPrevItems = () => {
-
-
-    setCurrent(current == 1 ?5 : (prevValue) => prevValue -1);
-   }
-
-   const showNextItems = () => {
-
-
-    setCurrent(current == 6 ?1 : (prevValue) => prevValue +1);
-   }
-
+  
+  const ref = useRef<HTMLDivElement>(document.createElement("div"));
+  const scrollRight = () => {
+    ref.current.scrollBy({left: 750, behavior: 'smooth'});
+  };
+  const scrollLeft = () => {
+    ref.current.scrollBy({left: -750, behavior: 'smooth'});
+  };
 
   return (
-  
-
-    <div className="photoSlider">
-       <FaArrowAltCircleLeft onClick={showPrevItems}></FaArrowAltCircleLeft>
-      <FaArrowAltCircleRight onClick={showNextItems}></FaArrowAltCircleRight>
-      {data.slice(0 , current).map((item:any,index:number) => (
-          <img className="image" src={require("../images/"+item)} key={index}/>
-      ))}
-     
-    </div>
+      <div ref={ref} className="photoSlider" >
+        {data.map((item:any,index:number) => (
+            <img className="image" src={require("../images/"+item)} key={index}/>
+        ))}
+        <a onClick={scrollLeft} className="leftIcon"><img style={{width:'5rem'}} src={left}/></a>
+        <a onClick={scrollRight} className="rightIcon"><img style={{width:'5rem'}} src={right}/></a>
+      </div>
   )
 }
 

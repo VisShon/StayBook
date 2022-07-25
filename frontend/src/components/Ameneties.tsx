fc9cec389d32d814b39f90976d355d0a33beffee
Anjanay Raina <anjanayraina326@gmail.com>
2022-07-25 14:46:13 +0530

    amenities

--- frontend/src/components/Ameneties.tsx
+++ frontend/src/components/Ameneties.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 import '../styles/Amneties.scss';
 
 function Ameneties({data}:any) {
+
+  // const [items , setItems]  = useState([]);
+  
+  const [visible , setVisible]  = useState(3);
+
+   const showMoreItems = () => {
+
+
+    setVisible((prevValue) => prevValue +20);
+   }
+
+
   return (
     <div className="details">Amenities:
 
-      {data.map((item:any,index:number) => (
-        <li className="amenities" key={index}>{item}</li>
+      {
+      data.slice(0 , visible).map((item:any,index:number) => (
+        <h1 className="amenities" key={index}>{index + 1} {item}</h1>
       ))}
-    
+    <button onClick={showMoreItems}>Load More</button>
     </div>
+
+
   )
+
+  
 }
 
 export default Ameneties
--- frontend/src/components/PhotoSlider.tsx
+++ frontend/src/components/PhotoSlider.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState , useEffect} from 'react';
 import '../styles/PhotoSlider.scss'
-
+import {FaArrowAltCircleRight , FaArrowAltCircleLeft} from 'react-icons/fa';
 function PhotoSlider({data}:any) {
+
+  const [current , setCurrent]  = useState(0);
+
+  const showPrevItems = () => {
+
+
+    setCurrent(current == 1 ?5 : (prevValue) => prevValue -1);
+   }
+
+   const showNextItems = () => {
+
+
+    setCurrent(current == 6 ?1 : (prevValue) => prevValue +1);
+   }
+
+
   return (
+  
+
     <div className="photoSlider">
-      {data.map((item:any,index:number) => (
+       <FaArrowAltCircleLeft onClick={showPrevItems}></FaArrowAltCircleLeft>
+      <FaArrowAltCircleRight onClick={showNextItems}></FaArrowAltCircleRight>
+      {data.slice(0 , current).map((item:any,index:number) => (
           <img className="image" src={require("../images/"+item)} key={index}/>
       ))}
+     
     </div>
   )
 }

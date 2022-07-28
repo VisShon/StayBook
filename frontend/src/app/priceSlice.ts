a90509ded313e9d86a2ddf20d56657d479bd25ab
VisShon <vishnu20414@iiitd.ac.in>
2022-07-29 03:46:14 +0530

    updates

--- frontend/src/app/priceSlice.ts
+++ frontend/src/app/priceSlice.ts
@@ -3,19 +3,21 @@ import { createSlice } from '@reduxjs/toolkit'
 export const priceSlice = createSlice({
     name:'totalPrice',
     initialState:{
-        value:0
+        value:0,
+        children:0
     },
     reducers:{
         updatePrice: (state:any,action:any) => {
             state.value = action.payload;
         },
-        increasePrice: (state:any,action:any) => {
-            state.value += action.payload;
+        addChild: (state:any) => {
+            state.children += 1;
+            state.value += state.children*500;
         }
     }
     
 })
 
-export const {updatePrice,increasePrice} = priceSlice.actions;
+export const {updatePrice,addChild} = priceSlice.actions;
 
 export default priceSlice.reducer;
--- frontend/src/components/SelectedPlan.tsx
+++ frontend/src/components/SelectedPlan.tsx
@@ -1,17 +1,34 @@
 import React,{useState} from 'react'
 import '../styles/BookingCard.scss'
-import { Picklist,Option } from 'react-rainbow-components';
 import { useAppDispatch } from '../app/hooks';
 import { removePlan } from '../app/planSlice';
+import { addChild } from '../app/priceSlice';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
 const dropdown = {
   width: '4.5rem',
   marginLeft: '0.5rem'
 };
 
 
+
 function SelectedPlan({title}:any) {
   
+  const [children, setChildren] = React.useState('0');
+  const [adults, setAdults] = React.useState('0');
+
   const dispatch = useAppDispatch();
+
+  const childChange = (event: SelectChangeEvent) => {
+    setChildren(event.target.value as string);
+    dispatch(addChild())
+  };
+  const adultsChange = (event: SelectChangeEvent) => {
+    setAdults(event.target.value as string);
+  };
+
   const onClickHandler = () =>{
     dispatch(removePlan(title));
   }
@@ -26,17 +43,27 @@ function SelectedPlan({title}:any) {
       <div className="wrapper">
           <div className="input">
             Adults
-            <Picklist style={dropdown} placeholder="1">
-                    <Option name="option 1" label="1" />
-                    <Option name="option 2" label="2" />
-            </Picklist>
+            <FormControl sx={{ m: 1, minWidth: 50,  }} size="small">
+                <Select
+                  value={adults}
+                  onChange={adultsChange}
+                >
+                  <MenuItem value={0}>0</MenuItem>
+                  <MenuItem value={1}>1</MenuItem>
+                </Select>
+            </FormControl>
           </div>
           <div className="input">
             Child
-            <Picklist style={dropdown} placeholder="0">
-                    <Option name="option 1" label="0" />
-                    <Option name="option 2" label="1" />
-            </Picklist>
+            <FormControl sx={{ m: 1, minWidth: 50 }} size="small">
+              <Select
+                value={children}
+                onChange={childChange}
+              >
+                <MenuItem value={0}>0</MenuItem>
+                <MenuItem value={1}>1</MenuItem>
+              </Select>
+            </FormControl>
           </div>
           <div>
             Room 1

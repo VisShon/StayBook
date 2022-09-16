623642f38248cc365f0a18522533d467df9704fa
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2022-09-17 01:44:19 +0530

    updates

--- frontend/src/components/home/sections/Plans.tsx
+++ frontend/src/components/home/sections/Plans.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import React from 'react';
+import plans from '../../../images/plan.svg'
+import '../../../styles/home/Plans.scss';
 function Plans() {
   return (
-    <div>Plans</div>
+    <div className="plansBody">
+      <img src={plans} />
+      <div className="centerCard">
+        <h2>Plan an Unforgettable Experience in Staybook!</h2>
+        <p>We can help you fit your stay and experience within your allotted budget.</p>
+        <a href="tel: +91-8373929299" className='contact'>Call for Support: +91-8373929299</a>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/home/sections/ToursandTravels.tsx
+++ frontend/src/components/home/sections/ToursandTravels.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import tourImage from '../../../images/travel.svg'
 import '../../../styles/home/ToursandTravels.scss'
 function ToursandTravels() {
+
   return (
     <>
         <img className='toursImage' src={tourImage}/>
         <div className='toursContent'>
-                <h2 className='title'> Tours and Travels</h2>
+                <h2> Tours and Travels</h2>
                 <p>Staybook offers customised family trips tailored 
                 to the needs of any family, large or small, with youngsters or the 
                 elderly. It's a fantastic chance to spend quality time together in a fantastic 
                 location.</p>
-                <div className='button'>Sign Up</div>
+                <div className='button'>Find more</div>
         </div>
     </>
   )

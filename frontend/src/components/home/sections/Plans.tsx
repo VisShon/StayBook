a3eb6a61bb4745f5608eaef71338c5690001fe7f
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2022-10-13 10:10:43 +0530

    updates

--- frontend/src/components/home/sections/Plans.tsx
+++ frontend/src/components/home/sections/Plans.tsx
@@ -1,19 +1,17 @@
-import React,{useEffect,useRef} from 'react';
+import React,{useEffect} from 'react';
 import plans from '../../../images/plan.svg'
 import '../../../styles/home/Plans.scss';
 import {motion,useAnimation} from 'framer-motion'
 import { useInView } from "react-intersection-observer";
 
 const boxVariant = {
-  visible: { opacity: 1, translateY: 0, transition: { duration: 1 } },
-  hidden: { opacity: 0, translateY: '5vw' }
+  visible: { opacity: 1, translateX: 0, transition: { duration: 2 } },
+  hidden: { opacity: 0, translateX: '10vw' }
 };
 
 function Plans() {
   const control = useAnimation();
   const [ref, inView] = useInView();
-  const [observer,observerInView] =useInView();
-
 
   useEffect(() => {
     if (inView) {
@@ -21,35 +19,21 @@ function Plans() {
     } else {
       control.start("hidden");
     }
-    let animationObject =  document.getElementById("image");
-    if(observerInView) animationObject!.className='bgImageFull'
-    else animationObject!.className='bgImageNew';
-
-  }, [control,inView,observerInView]);
+  }, [control, inView]);
   
   return (
-    <div className="container" >
-      <img src={plans} id='image'/>
-      <div ref={observer} className='observer'></div>
-      <div className="plansBody">
-        <div>
-          <motion.div initial='hidden'
-                ref={ref}
-                variants={boxVariant}
-                animate={control}>STAYBOOK</motion.div>
-          <img src={plans} />
-        </div>
-        <div>
-          <img src={plans}/>
-          <motion.div initial='hidden'
-                variants={boxVariant}
-                animate={control}>FIND</motion.div>
-        </div>
-          <motion.div initial='hidden'
-                variants={boxVariant}
-                animate={control}>UNFORGETTABLE EXPERIENCES</motion.div>
+    <motion.div className="plansBody"
+              initial='hidden'
+              ref={ref}
+              variants={boxVariant}
+              animate={control}>
+      <img src={plans} />
+      <div className="centerCard">
+        <h2>Plan an Unforgettable Experience in Staybook!</h2>
+        <p>We can help you fit your stay and experience within your allotted budget.</p>
+        <a href="tel: +91-8373929299" className='contact'>Call for Support: +91-8373929299</a>
       </div>
-    </div>
+    </motion.div>
   )
 }
 

0e193460ad7ab4171b25c0f24ad26b1cf1d177bf
Naman Kaushik <kaushiknaman.dk@gmail.com>
2023-01-11 22:40:37 +0530

    blog page edits

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import '../styles/home/Blogs.scss'
-import client from '../client'
-import {Helmet} from 'react-helmet'
-import BlogCard from '../components/BlogCard'
+import React, { useState, useEffect } from "react";
+import "../styles/home/Blogs.scss";
+import client from "../client";
+import { Helmet } from "react-helmet";
+import BlogCard from "../components/BlogCard";
+import logo from "../images/logo.png";
 function Blog() {
-    const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        client
-            .fetch(
-                `*[_type == "post"] {
+  useEffect(() => {
+    client
+      .fetch(
+        `*[_type == "post"] {
         title,
         slug,
         body,
@@ -21,27 +22,42 @@ function Blog() {
           alt
         }
       }`
-            )
-            .then((data) => setPosts(data))
-    }, [])
+      )
+      .then((data) => setPosts(data));
+  }, []);
 
-    return (
-        <>
-            <Helmet>
-                <title>StayBook Blogs</title>
-                <meta name="description" content="StayBook Booking engine for Hotels enabled with high speed wifi throughout. There are different wifi connections on different floors. The guest can find the wifi passwords on the wifi cards inside of their rooms we have a specialized work station for our guests with high speed cables. Delhi hotels" />
-            </Helmet>
-            <div className="blogHeader">
-                <h4 className="heading">Blog page</h4>
-            </div>
+  return (
+    <>
+      <Helmet>
+        <title>StayBook Blogs</title>
+        <meta
+          name="description"
+          content="StayBook Booking engine for Hotels enabled with high speed wifi throughout. There are different wifi connections on different floors. The guest can find the wifi passwords on the wifi cards inside of their rooms we have a specialized work station for our guests with high speed cables. Delhi hotels"
+        />
+      </Helmet>
+      <div className="blogHeader">
+        <img src={logo} alt="StayBook Hotels" />
+        <h4 className="heading">Blog page</h4>
 
-            <div className="blogsContainer">
-                {posts.map((post: any) => (
-                    <BlogCard post = {post}/>
-                ))}
-            </div>
-        </>
-    )
+        <span className="sr-only">Search</span>
+        <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+          {/* <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg> */}
+        </span>
+        <input
+          className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm blogSearch"
+          placeholder="Search blogs"
+          type="text"
+          name="search"
+        />
+      </div>
+
+      <div className="blogsContainer">
+        {posts.map((post: any) => (
+          <BlogCard post={post} />
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Blog
+export default Blog;

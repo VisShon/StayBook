4994a4ff3047ef4d4041af0faaebdc93302da8ec
Naman Kaushik <kaushiknaman.dk@gmail.com>
2023-01-13 12:34:54 +0530

    updated build

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -40,15 +40,15 @@ function Blog() {
         <h4 className="heading">Blog page</h4>
 
         <span className="sr-only">Search</span>
-        <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+        {/* <span className="absolute inset-y-0 left-0 flex items-center pl-2"> */}
           {/* <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg> */}
-        </span>
-        <input
+        {/* </span> */}
+        {/* <input
           className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm blogSearch"
           placeholder="Search blogs"
           type="text"
           name="search"
-        />
+        /> */}
       </div>
 
       <div className="blogsContainer">

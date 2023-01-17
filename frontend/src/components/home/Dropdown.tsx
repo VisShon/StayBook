1e27e6992f4cc1680f334d90946d6eeebe6727c9
Naman Kaushik <kaushiknaman.dk@gmail.com>
2023-01-17 13:31:22 +0530

    fix:flickering of navbar

--- frontend/src/components/home/Dropdown.tsx
+++ frontend/src/components/home/Dropdown.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import '../../styles/NavBar.scss'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import client from '../../client'
 
 function Dropdown() {
@@ -18,34 +18,35 @@ function Dropdown() {
 
     const nav = useNavigate()
     return (
-        <ul>
-            <li>
-                <a  href='/hotels' 
-                    style={{textDecoration:'none',color:'black'}} 
-                    aria-haspopup="true">
-                        Hotels</a>
-                <ul className="dropdown" aria-label="submenu">
-                    {Object.values(data).map((item: any, i: any) => (
-                        <li
-                            className="hotels"
-                            key={i}
-                            onClick={() => nav(`/hotels/${item.slug.current}`)}
-                        >
-                            <a 
-                                href={`/hotels/${item.slug.current}`}
-                                style={{
-                                    textDecoration: 'none',
-                                    color: 'black',
-                                }}
-                            >
-                                {item.name}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </li>
-        </ul>
-    )
+      <ul>
+        <li>
+          
+
+          <Link to="/hotels" className="item">
+            Hotels
+          </Link>
+          <ul className="dropdown" aria-label="submenu">
+            {Object.values(data).map((item: any, i: any) => (
+              <li
+                className="hotels"
+                key={i}
+                onClick={() => nav(`/hotels/${item.slug.current}`)}
+              >
+                <a
+                  href={`/hotels/${item.slug.current}`}
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                  }}
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </li>
+      </ul>
+    );
 }
 
 export default Dropdown
--- frontend/src/components/home/Navbar.tsx
+++ frontend/src/components/home/Navbar.tsx
@@ -1,99 +1,112 @@
-import React, {useState, useEffect,useRef } from 'react';
-import { Link } from 'react-router-dom';
-import { useJwt } from 'react-jwt';
-import logo from '../../images/logo.png';
-import '../../styles/NavBar.scss';
-import Dropdown from './Dropdown';
-import user from '../../images/user.svg';
-import { useScroll } from 'framer-motion';
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useJwt } from "react-jwt";
+import logo from "../../images/logo.png";
+import "../../styles/NavBar.scss";
+import Dropdown from "./Dropdown";
+import user from "../../images/user.svg";
+import { useScroll } from "framer-motion";
 
 function Navbar() {
+  const { scrollYProgress } = useScroll();
+  const navbar = useRef<HTMLDivElement>(null);
+  const hamburger = useRef<HTMLButtonElement>(null);
+  const userToken = sessionStorage.getItem("user");
+  const { decodedToken, isExpired }: any = useJwt(userToken!);
+  const [isOpen, setIsOpen] = useState(false);
+  var prevScrollpos = window.pageYOffset;
+  useEffect(() => {
+    window.onscroll = function () {
+      var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos >= currentScrollPos) {
+      
+          navbar.current!.className = "navbar";
+          console.log("scrolling up");
+      } else {
+          navbar.current!.className = "navbar-mini";
+          console.log("scrolling down");
+      }
+      prevScrollpos = currentScrollPos;
+    };
+    // scrollYProgress.onChange((latest) => {
+    //     if(latest<scrollYProgress.getPrevious()){
+    //         navbar.current!.className='navbar';
+    //     }
+    //     else if(latest>=scrollYProgress.getPrevious()){
+    //         navbar.current!.className='navbar-mini';
+    //     };
+    // })
+    window.addEventListener('mousemove',()=>{
+        navbar.current!.className='navbar';
+    })
+  }, []);
 
-    const {scrollYProgress} = useScroll()
-    const navbar = useRef<HTMLDivElement>(null)
-    const hamburger = useRef<HTMLButtonElement>(null)
-    const userToken = sessionStorage.getItem('user');
-    const { decodedToken, isExpired }: any = useJwt(userToken!);
-    const [isOpen,setIsOpen] = useState(false);
-
-    useEffect(() => {
-        scrollYProgress.onChange((latest) => {
-            if(latest<scrollYProgress.getPrevious()){
-                navbar.current!.className='navbar';
-            }
-            else{
-                navbar.current!.className='navbar-mini';
-            };
-        })
-        window.addEventListener('mousemove',()=>{
-            navbar.current!.className='navbar';
-        })
-    },[])
-
-    const collapse = () => {
-        if(hamburger.current!.classList.contains('is-active')){
-            hamburger.current!.classList.remove('is-active')
-            setIsOpen(false)
-        }
-        else{
-            hamburger.current!.classList.add('is-active')
-            setIsOpen(true)
-        }
+  const collapse = () => {
+    if (hamburger.current!.classList.contains("is-active")) {
+      hamburger.current!.classList.remove("is-active");
+      setIsOpen(false);
+    } else {
+      hamburger.current!.classList.add("is-active");
+      setIsOpen(true);
     }
+  };
 
+  return (
+    <div className="navbar" ref={navbar}>
+      <Link to="/" className="logo">
+        <img src={logo} className="logo" alt={"StayBook Hotels"} />
+      </Link>
+          <div className="TourPackage">
+              
+        <Dropdown />
+        <Link to="/packages" className="item">
+          Tour Packages
+        </Link>
+        <Link to="/blogs" className="item">
+          Blogs
+        </Link>
+        <Link to="/contactus" className="item">
+          Contact Us
+        </Link>
+      </div>
 
-    return (
-        <div className="navbar" ref={navbar}>
-            <Link to="/" className="logo">
-                <img src={logo} className="logo" alt={'StayBook Hotels'}/>
-            </Link>
-            <div className="TourPackage">
-                <Dropdown />
-                <Link to="/packages" className="item">
-                    Tour Packages
-                </Link>
-                <Link to="/blogs" className="item">
-                    Blogs
-                </Link>
-                <Link to="/contactus" className="item">
-                    Contact Us
-                </Link>
-            </div>
-            
+      <Link to="/profile" className="logo">
+        <img
+          src={decodedToken ? decodedToken.picture : user}
+          className="profilepic"
+          alt={"StayBook Hotels"}
+        />
+      </Link>
 
-            <Link to="/profile" className="logo">
-                <img
-                    src={decodedToken ? decodedToken.picture : user}
-                    className="profilepic"
-                    alt={'StayBook Hotels'}
-                />
-            </Link>
+      <div className="navMobile">
+        <button
+          className="hamburger hamburger--collapse"
+          ref={hamburger}
+          type="button"
+          onClick={collapse}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+      </div>
 
-            <div className="navMobile">
-                <button className="hamburger hamburger--collapse" 
-                        ref={hamburger}
-                        type="button" 
-                        onClick={collapse}>
-                    <span className="hamburger-box">
-                        <span className="hamburger-inner"></span>
-                    </span>
-                </button>
-            </div>
-
-            {isOpen&&<div className="navMobile-box">
-                <Link className='item' to="/packages">
-                    Tour Packages
-                </Link>
-                <Link className='item' to="/blogs">
-                    Blogs
-                </Link>
-                <Link className='item' to="/contactus">
-                    Contact Us
-                </Link>
-                <Dropdown/>
-            </div>}
+      {isOpen && (
+        <div className="navMobile-box">
+          <Link className="item" to="/packages">
+            Tour Packages
+          </Link>
+          <Link className="item" to="/blogs">
+            Blogs
+          </Link>
+          <Link className="item" to="/contactus">
+            Contact Us
+          </Link>
+          <Dropdown />
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;

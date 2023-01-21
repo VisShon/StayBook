2d1f5e8e167dfd755ac8487551597a6abb7a63a9
Naman Kaushik <kaushiknaman.dk@gmail.com>
2023-01-21 18:31:03 +0530

    blog card update

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,15 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import "../styles/home/Blogs.scss";
 import React from "react";
 
 export default function BlogCard(props: any) {
+  const nav = useNavigate();
+  const showBlogDetails = () => {
+    nav(`/blogs/${props.post.slug.current}`);
+  }
   return (
-      <article key={props.post.slug.current} className="blogCard">
+    <article key={props.post.slug.current} className="blogCard" onClick={ showBlogDetails}>
         <img src={props.post.mainImage.asset.url} alt={props.post.title} />
         <h3>{props.post.title}</h3>
-    <Link className="button" to={`/blogs/${props.post.slug.current}`}>
+    {/* <Link className="button" to={`/blogs/${props.post.slug.current}`}>
       Read Full Article
-    </Link>
+    </Link> */}
       </article>
 
     // <div className="container">
